576c57817ac354ee9689acf5162a4e4183b5d3be
Alexandru Simionov <alexandru.simionov.ro@gmail.com>
2022-10-17 14:07:10 +0300

    Added email notification!

--- testJenkins.sh
+++ testJenkins.sh
@@ -3,4 +3,4 @@
 echo "This script is on my GitHub Repo"
 uname -a | awk '{print $2}' && date
 echo "This was added after setting the webhook!"
-echo "This was added after setting the webhook!"
+echo "Added email notification for every build run!"
